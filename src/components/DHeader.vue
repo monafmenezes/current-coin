f357783df323125c7808fa477b22cd535e1bb79c
Monalisa Menezes <monalisa.menezes@onesight.com.br>
2022-11-02 05:40:41 -0300

    feat: Add header and footer components

---
+++ src/components/DHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="h-1/5 container mx-auto flex justify-center items-center animate-pulse"
+  >
+    <h2 class="text-7xl text-gold text-white">Current</h2>
+    <img src="../static/logo.png" class="w-28" />
+    <h2 class="text-7xl text-gold">Coin</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: "DHeader",
+};
+</script>
