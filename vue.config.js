9af3b34d2efb2a7c9daa0158adb4b0626498f0ed
Monalisa Menezes <monalisa.menezes@onesight.com.br>
2022-11-04 17:23:07 -0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/current-coin/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/current-coin" : "/",
 });
